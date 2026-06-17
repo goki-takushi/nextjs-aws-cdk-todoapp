b015cfcb7cfd6fa524cb6e012331c7452cc9b1da
goki-takushi <293592760+goki-takushi@users.noreply.github.com>
2026-06-17 22:10:22 +0900

    feat: prismaセットアップ

---
+++ next-app/src/lib/db.ts
@@ -0,0 +1,23 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma/client";
+
+// グローバル変数として定義（開発環境でのホットリロード対策）
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// 1. PostgreSQLの接続プールを作成
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+// 2. Prisma用のDriver Adapterを作成
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log: ["query", "error", "warn"], // クエリログを出力
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
